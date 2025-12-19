b1e1c5e20a7861b538d388fc7a0a2968d604c12c
Shankar-Sala <shankarsala9995@gmail.com>
2025-12-19 17:56:56 +0530

    feat(auth): complete Clerk authentication setup

--- frontend-client/src/App.jsx
+++ frontend-client/src/App.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './Pages/Login'
+import Feed from './Pages/Feed'
+import Messages from './Pages/Messages'
+import ChatBox from './Pages/ChatBox'
+import Connections from './Pages/Connections'
+import Discover from './Pages/Discover'
+import Profile from './Pages/Profile'
+import CreatePost from './Pages/CreatePost'
+import {useUser} from '@clerk/clerk-react'
+import Layout from './Pages/Layout'
+
 
 const App = () => {
+  const {user} = useUser()
   return (
-    <div>App</div>
+    <>
+    <Routes>
+      <Route path='/' element={!user? <Login /> : <Layout/>}>
+        <Route  index element={<Feed />} />
+        <Route  path='messages' element={<Messages />} />
+        <Route  path='messages/:userId' element={<ChatBox />} />
+        <Route  path='connection' element={<Connections />} />
+        <Route  path='discover' element={<Discover />} />
+        <Route  path='profile' element={<Profile />} />
+        <Route  path='profile/:profileId' element={<Profile />} />
+        <Route  path='create-post' element={<CreatePost />} />
+      </Route>
+    </Routes>
+    </>
   )
 }
 
