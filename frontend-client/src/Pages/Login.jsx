014f0dcd1f5f20dab9b4c75afb3674f3674f3e72
Shankar-Sala <shankarsala9995@gmail.com>
2025-12-26 17:32:18 +0530

    favicon

--- frontend-client/src/Pages/Login.jsx
+++ frontend-client/src/Pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { assets } from "../assets/assets";
 import { Star } from "lucide-react";
-import {SignIn} from '@clerk/clerk-react'
+import { SignIn } from "@clerk/clerk-react";
 
 const Login = () => {
   return (
@@ -15,7 +15,13 @@ const Login = () => {
 
       {/* left side : Branding  */}
       <div className="flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:p1-40">
-        <img src={assets.logo} alt="" className="h-12 object-contain" />
+        {/* <img src={assets.logo} alt="" className="h-12 object-contain" /> */}
+        <img
+  src="/logo.png"
+  alt="ProConnect Logo"
+  className="w-32 sm:w-40 md:w-48 lg:w-56 h-auto object-contain"
+/>
+
         <div>
           <div className="flex items-center gap-3 mb-4 max-md:mt-10">
             <img src={assets.group_users} alt="" className="h-8 md:h-10" />
@@ -33,8 +39,12 @@ const Login = () => {
               <p>Used by 12K+ developers</p>
             </div>
           </div>
-          <h1 className="text-3xl md:text-6xl md:pb-2 font-bold bg-gradient-to-r from-indigo-950 to-indigo-800 bg-clip-text text-transparent">More than just friends truly connect</h1>
-          <p className="text-xl md:text-3xl text-indigo-900 max-w-72 md:max-w-md">connect with global community on ProConnect</p>
+          <h1 className="text-3xl md:text-6xl md:pb-2 font-bold bg-gradient-to-r from-indigo-950 to-indigo-800 bg-clip-text text-transparent">
+            More than just friends truly connect
+          </h1>
+          <p className="text-xl md:text-3xl text-indigo-900 max-w-72 md:max-w-md">
+            connect with global community on ProConnect
+          </p>
         </div>
         <span className="md:h-10"></span>
       </div>
