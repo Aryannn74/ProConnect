1481d4ae7b8c6d6a7b554ea5f97d36956a94ded0
Shankar-Sala <shankarsala9995@gmail.com>
2025-12-28 03:04:22 +0530

    imagekit

---
+++ server/controllers/userController.js
@@ -0,0 +1,95 @@
+import imagekit from "../configs/imageKit.js";
+import User from "../models/User.js";
+import fs from "fs";
+import path from "path";
+import { format } from "path";
+
+//Get User Data Using userID
+export const getUserData = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+    res.json({ success: true, user });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Update User Data
+export const updateUserData = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { username, bio, location, full_name } = req.body;
+    const tempUser = await User.findById(userId);
+
+    !username && (username = tempUser.username);
+
+    if (tempUser.username !== username) {
+      const user = User.findOne({ username });
+      if (user) {
+        // we will not change the username if it is already taken
+        username = tempUser.username;
+      }
+    }
+
+    const updatedData = {
+      username,
+      bio,
+      location,
+      full_name,
+    };
+
+    const profile = req.files.profile && req.files.profile[0];
+    const cover = req.files.cover && req.files.cover[0];
+
+    if (profile) {
+      const buffer = fs.readFileSync(profile.path);
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: profile.originalname,
+      });
+
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          { quality: "auto" },
+          { format: "webp" },
+          { width: "512" },
+        ],
+      });
+      updatedData.profile_picture = url;
+    }
+
+    if (cover) {
+      const buffer = fs.readFileSync(cover.path);
+      const response = await imagekit.upload({
+        file: buffer,
+        fileName: profile.originalname,
+      });
+
+      const url = imagekit.url({
+        path: response.filePath,
+        transformation: [
+          { quality: "auto" },
+          { format: "webp" },
+          { width: "1280" },
+        ],
+      });
+      updatedData.cover_photo = url;
+    }
+
+    const user = await User.findByIdAndUpdate(UserID, updatedData, {
+      new: true,
+    });
+
+    res.json({success: true, user, message: 'Profile updated successfully'})
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -2,9 +2,9 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
-import {inngest, functions} from "./inngest/index.js"
-import {serve} from 'inngest/express'
-
+import { inngest, functions } from "./inngest/index.js";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
 
@@ -12,10 +12,12 @@ await connectDB();
 
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware());
 
-app.get('/', (req, res) => res.send("✅ Server is running"));
-app.use('/api/inngest', serve({ client: inngest, functions}));
+app.get("/", (req, res) => res.send("✅ Server is running"));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`)
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on http://localhost:${PORT}`)
 );
